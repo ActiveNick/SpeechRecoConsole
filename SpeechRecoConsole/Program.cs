9e0c36ea869e1db4b9762ace3287f43b9368e29c
Nick Landry <nick.landry@microsoft.com>
2018-07-18 16:00:26 -0400

    Add project files.

---
+++ SpeechRecoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeechRecoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
